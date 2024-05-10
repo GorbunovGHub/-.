d70fa7dc73b6ada041cde4b8da55dc6eeb16e5df
GorbunovGHub <pavlixrus@mail.ru>
2024-05-10 22:25:42 +0300

    Add files via upload

---
+++ Homework_17.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self):
+        self.floors = 20
+
+house = House()
+
+numberOfFloors = 10
+
+for numberOfFloors in range(numberOfFloors):
+    print('Текущий этаж равен', numberOfFloors)
